96a391e558bd4cc2460141c6a6dfe7e22f681c06
Isaias <xuxogh2015@gmail.com>
2025-02-02 12:56:43 -0500

    creacion de variable tipo array

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//variables
+let amigosIngresados = [];
+let entrada = 0;
+
+function agregarAmigo(){
+    entrada = document.querySelector("input").value;
+    console.log(entrada);
+
+    
+
+    if 
+}
