1c2872b445f68e3e4abdc24ab9ef09a4308aebd1
Isaias <xuxogh2015@gmail.com>
2025-02-02 14:54:07 -0500

    crear la funcion actualizar lista de amigos

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ let entrada = 0;
 function agregarAmigo(){
     entrada = document.querySelector("input").value;// Esto captura el valor de entrada del elemento input
     console.log(entrada);
-  // validamos la entrada de usuario y actualizamos la lista y limpiamos el input
+  // validamos la entrada de usuario ,actualizamos la lista y limpiamos el input
     if (entrada == ""){
         alert("Por favor inserta un nombre");
     } else{
         amigosIngresados.push(entrada);
-        limpiar = document.querySelector("input").value = "";
         console.log(amigosIngresados);
+        document.querySelector("input").value = "";
+        ActualizarAmigos();//llamamos a la funcion
+        return
     }    
+}
+
+function ActualizarAmigos (){
+    lista = document.getElementById("listaAmigos") // captura la lista ul del html
+    lista.innerHTML = "";// limpia los datos de la  lista actual
+
+    for (let i = 0; i < amigosIngresados.length; i++) { // bucle que recorre la variable tipo array
+ 
+        let amigo = document.createElement("li");// esto crea un elemento li 
+        amigo.textContent = amigosIngresados[i];// aqui se agrega el contenido de la variable al elemento li
+        lista.appendChild(amigo);//esto agrega el elemento li al elemnto ul
+ 
+    }
+    
 }
